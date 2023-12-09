615de4911512c76c4ba1561c953db2f580a5f814
LJY_LU <1005058349@qq.com>
2023-12-09 20:51:07 +0800

    first commit

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+"""
+# GitHub Poster
+
+Welcome! It's a tool to help you Share Your GitHub Repos!
+""" 
+
+# 创建链接获取GitHub仓库
+github_repo = st.text_input("Please type GitHub Repo's Url here")
